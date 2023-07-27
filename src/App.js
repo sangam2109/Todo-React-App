abc39db8efe0bcabbd1c893488f2ea76bc921543
Sangam Arora <arorasam2109@gmail.com>
2023-07-27 14:50:24 +0530

    -m

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import Home from './Homepage/Home';
-
+import React, {useState,useEffect} from 'react';
+import WelcomePage from './components/WelcomePage/welcome';
 
 function App() {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  useEffect(() => {
+
+    const timer = setTimeout(() => {
+      setShowWelcome(false);
+    }, 5000);
+
+    // Clear the timer on unmount to avoid potential memory leaks
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <main className='main-container'>
-      <Home />
+      <div className="app">
+        {showWelcome ? <WelcomePage /> : <Home />}
+      </div>
     </main>
   );
 }
---
+++ src/components/WelcomePage/welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Welcome.css';
+
+const Welcome = () => {
+    return (
+        <div className="welcome-page">
+            <div className="welcome-content">
+                <h1 className="welcome-text">Welcome to My Todo App!</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Welcome;
